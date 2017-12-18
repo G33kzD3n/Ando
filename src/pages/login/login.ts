7a1a13fac3b105ebce6c3f40298f2dc230f114ae
nadeem09wani <nadeem.09wani@gmail.com>
2017-12-18 22:29:19 +0530

    added doc block as coments for member functions in login.ts

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,7 +8,7 @@ import { SignupPage } from '../signup/signup';
 @IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
   users = [{
@@ -24,7 +24,7 @@ export class LoginPage {
     email: "sami@gmail.com",
     mobileNo: 9018556691,
     password: "stranger"
-
+    
   },
   {
     id: 3,
@@ -33,87 +33,91 @@ export class LoginPage {
     mobileNo: 9018556691,
     password: "stranger"
   }
-  ];
-    public loginForm : FormGroup;
-    public Message   : any;
-    public pageTitle : string;
-    public userName  : string;
+];
+public loginForm : FormGroup;
+public Message   : any;
+public pageTitle : string;
+public userName  : string;
 
   constructor(public navCtrl        : NavController,
-              public navParam       : NavParams,
-              public formbuilder    : FormBuilder)
-  {
+    public navParam       : NavParams,
+    public formbuilder    : FormBuilder)
+    {
       this.loginForm = formbuilder.group({
         "email"     : ['',Validators.compose([
-                          Validators.required,
-                          Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])],
-        "password"  : ['',Validators.compose([
-                          Validators.required,
-                          Validators.minLength(8)
-                      ])], 
-            });
-  }
-
-  ionViewWillEnter(): void {
-    this.pageTitle = "Login Here";
-    this.Message = this.navParam.get('Message');
-  }
- 
-  /**
-   * @param user is user object recieved from form .
-   * @returns A boolean response , True if user found, else default value.
-   */
-  public isUserFound(user:any) {
-    var UserFound: boolean = false;
-    this.users.forEach(obj => {
-      if (obj.email === user.email  && obj.password === user.password) {
-        UserFound = true;
-        return UserFound;
+          Validators.required,
+          Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)])],
+          "password"  : ['',Validators.compose([
+            Validators.required,
+            Validators.minLength(8)
+          ])], 
+        });
       }
-    });
-    return UserFound; //default value returned
-  } 
-
-  /**
-   * @param value input form values 
-   * Logs in authorised user to homePage 
-   * For unauthorised user prompts message at  login
-   */
-  onSubmit(value : any )
-  { 
-    let user = {
-      email    : this.loginForm.controls['email'].value,
-      password : this.loginForm.controls['password'].value
-    };   
-    if ( this.isUserFound(user) ){
-      this.userName = this.getUserName(user.email);
-      this.navCtrl.setRoot(HomePage ,{
-      pageTitle : "Home Page",
-      userName : this.userName
-    });
-  }
-  else{
-      this.navCtrl.setRoot(LoginPage,{
-        Message : "*Email/Password incorrect."
-      });
-  }
-}
-  forgotPassword() : void {
-      this.Message = "forgotPassword";
-    console.log(this.Message);
-  }
-  registerUser() : void {
-    this.navCtrl.push(SignupPage,{
-      'title' : "Sign" });
-  }
-
-public getUserName(key:string){
-  let name : string  ="";  
-  this.users.forEach( user => {
-    if(user.email === key){
-        return name=user.name;
-    }
-  });
-  return name;
- }
+      
+      ionViewWillEnter(): void {
+        this.pageTitle = "Login Here";
+        this.Message = this.navParam.get('Message');
+      }
+      
+      /**
+       * @param {any} user  is a object recieved from form .
+       * @returns A boolean response , True if user found, else default value.
+       */
+      public isUserFound(user:any) {
+        var UserFound: boolean = false;
+        this.users.forEach(obj => {
+          if (obj.email === user.email  && obj.password === user.password) {
+            UserFound = true;
+            return UserFound;
+          }
+        });
+        return UserFound; //default value returned
+      } 
+      
+      /**
+       * @param {any} value input form values 
+       * Logs in authorised user to homePage 
+       * For unauthorised user prompts message at  login
+       */
+      onSubmit(value : any )
+      { 
+        let user = {
+          email    : this.loginForm.controls['email'].value,
+          password : this.loginForm.controls['password'].value
+        };   
+        if ( this.isUserFound(user) ){
+          this.userName = this.getUserName(user.email);
+          this.navCtrl.setRoot(HomePage ,{
+            pageTitle : "Home Page",
+            userName : this.userName
+          });
+        }
+        else{
+          this.navCtrl.setRoot(LoginPage,{
+            Message : "*Email/Password incorrect."
+          });
+        }
+      }
+      forgotPassword() : void {
+        this.Message = "forgotPassword";
+        console.log(this.Message);
+      }
+      registerUser() : void {
+        this.navCtrl.push(SignupPage,{
+          'title' : "Sign" });
+        }
+        
+        /**
+         * @param {string} key usually email , 
+         * matches the key in users and returns the username of the record whose key matched
+         */
+        public getUserName(key : any){
+          let name : string  ="";  
+          this.users.forEach( user => {
+            if(user.email === key){
+              return name=user.name;
+            }
+          });
+          return name;
+        }
 }
