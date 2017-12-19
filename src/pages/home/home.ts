829bd64d4e2fbcbb61225c1d2268cb7f72c79374
= <=>
2017-12-19 18:00:02 +0530

    Add the menu functionality in homepage, also logout alert prompt. Codeformating with proper indentation. :)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,58 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-// import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { NavController, NavParams} from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
-import { LoginPage} from '../login/login';
+import { ProfilePage } from '../profile/profile';
+import { LoginPage } from "../login/login";
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: '../menu/menu.html'
 })
+
 export class HomePage {
   public pageTitle  : string;
-  public userName   : string;
-  content :any;
-  constructor(public navCtrl : NavController, public navParam : NavParams )
-  {
-  }
-  ionViewWillEnter(): void {
-    this.pageTitle = this.navParam.get('pageTitle');
-    this.userName=this.navParam.get('userName');
-  }
-  showProfile(){
-    this.pageTitle="Profile";
+  public pages      : Array<{title :string, component: any}>;
 
+  constructor(public navCtrl : NavController, public navParam : NavParams , public alertCtrl : AlertController)
+  { 
+    this.pageTitle ="Home";
+    this.pages = [
+      { title : 'Profile', component : ProfilePage},
+      { title : 'Logout',  component : LoginPage }
+    ];
   }
-  logout(){
-    this.navCtrl.setRoot(LoginPage);
-    // this.navCtrl.push(LoginPage);
+  
+  /** 
+   * @param page which is a object having title and component
+   * @returns dynamically loads the view of the object recieved.  
+   */
+  openPage(page){
+    if ( page.component == LoginPage ){
+      let confirm = this.alertCtrl.create({
+        title: 'Logout',
+        message: "Do you really want to logout?",
+        buttons: [
+          {
+            text: 'Disagree',
+            handler: () => {
+            }
+          },
+          {
+            text: 'Agree',
+            handler: () => {
+              this.navCtrl.setRoot(page.component, {
+                Message: "Thanks " + this.navParam.get('userName') + " ! Login again."
+              });
+            }
+          }
+        ]
+      });
+      confirm.present();
+    }
+    else
+     this.navCtrl.push(page.component,{
+      userName  : this.navParam.get('userName')
+    });
   }
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,25 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
+  public pageTitle: string;
+  public userName : string ;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.userName  = this.navParams.get('userName');
+    this.pageTitle = "Profile ";
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad ProfilePage');
+  // }
 
 }
