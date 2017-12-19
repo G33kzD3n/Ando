ec46b3c0b6c177f3ba3238b9b55dfc190a349da9
= <=>
2017-12-19 18:00:02 +0530

    Add the menu functionality in homepage, also logout alert prompt .

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,6 +10,7 @@ import { SignupPage } from '../signup/signup';
   selector: 'page-login',
   templateUrl: 'login.html'
 })
+
 export class LoginPage {
   users = [{
     id: 1,
@@ -34,14 +35,12 @@ export class LoginPage {
     password: "stranger"
   }
 ];
-public loginForm : FormGroup;
-public Message   : any;
-public pageTitle : string;
-public userName  : string;
+  public loginForm : FormGroup;
+  public Message   : any;
+  public pageTitle : string;
+  public userName  : string;
 
-  constructor(public navCtrl        : NavController,
-    public navParam       : NavParams,
-    public formbuilder    : FormBuilder)
+  constructor(public navCtrl : NavController, public navParam : NavParams, public formbuilder : FormBuilder)
     {
       this.loginForm = formbuilder.group({
         "email"     : ['',Validators.compose([
@@ -52,72 +51,71 @@ public userName  : string;
             Validators.minLength(8)
           ])], 
         });
-      }
+    }
       
-      ionViewWillEnter(): void {
-        this.pageTitle = "Login Here";
-        this.Message = this.navParam.get('Message');
+  ionViewWillEnter(): void {
+    this.pageTitle = "Login Here";
+    this.Message = this.navParam.get('Message');
+  }
+  /**
+   * @param {any} user  is a object recieved from form .
+   * @returns A boolean response , True if user found, else default value.
+   */
+  public isUserFound(user:any) {
+    var UserFound: boolean = false;
+    this.users.forEach(obj => {
+      if (obj.email === user.email  && obj.password === user.password) {
+        UserFound = true;
+        return UserFound;
       }
+    });
+    return UserFound; //default value returned
+  } 
       
-      /**
-       * @param {any} user  is a object recieved from form .
-       * @returns A boolean response , True if user found, else default value.
-       */
-      public isUserFound(user:any) {
-        var UserFound: boolean = false;
-        this.users.forEach(obj => {
-          if (obj.email === user.email  && obj.password === user.password) {
-            UserFound = true;
-            return UserFound;
-          }
-        });
-        return UserFound; //default value returned
-      } 
-      
-      /**
-       * @param {any} value input form values 
-       * Logs in authorised user to homePage 
-       * For unauthorised user prompts message at  login
-       */
-      onSubmit(value : any )
-      { 
-        let user = {
-          email    : this.loginForm.controls['email'].value,
-          password : this.loginForm.controls['password'].value
-        };   
-        if ( this.isUserFound(user) ){
-          this.userName = this.getUserName(user.email);
-          this.navCtrl.setRoot(HomePage ,{
-            pageTitle : "Home Page",
-            userName : this.userName
-          });
-        }
-        else{
-          this.navCtrl.setRoot(LoginPage,{
-            Message : "*Email/Password incorrect."
-          });
-        }
-      }
-      forgotPassword() : void {
-        this.Message = "forgotPassword";
-        console.log(this.Message);
-      }
-      registerUser() : void {
-        this.navCtrl.push(SignupPage,{
-          'title' : "Sign" });
-        }
+  /**
+   * @param {any} value input form values 
+   * Logs in authorised user to homePage 
+   * For unauthorised user prompts message at  login
+   */
+  onSubmit(value : any ){ 
+    let user = {
+      email    : this.loginForm.controls['email'].value,
+      password : this.loginForm.controls['password'].value
+    };   
+    if ( this.isUserFound(user) ){
+      this.userName = this.getUserName(user.email);
+      this.navCtrl.setRoot(HomePage ,{
+        pageTitle : "Home Page",
+        userName : this.userName
+      });
+    }
+    else{
+      this.navCtrl.setRoot(LoginPage,{
+        Message : "*Email/Password incorrect."
+      });
+    }
+  }
+  
+  forgotPassword() : void {
+    this.Message = "forgotPassword";
+    console.log(this.Message);
+  }
+
+  registerUser() : void {
+    this.navCtrl.push(SignupPage);
+  }
         
-        /**
-         * @param {string} key usually email , 
-         * matches the key in users and returns the username of the record whose key matched
-         */
-        public getUserName(key : any){
-          let name : string  ="";  
-          this.users.forEach( user => {
-            if(user.email === key){
-              return name=user.name;
-            }
-          });
-          return name;
-        }
+  /**
+   * @param {string} key usually email , 
+   * matches the key in users and returns the username of the record whose key matched
+   */
+  public getUserName(key : any){
+    let name : string  ="";  
+    this.users.forEach( user => {
+      if(user.email === key){
+        return name=user.name;
+      }
+    });
+    return name;
+  }
 }
