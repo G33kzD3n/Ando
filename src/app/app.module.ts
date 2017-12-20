9bfd4e73f5c77bd69d1140d9c7beadb6ffc304d4
= <nadeem.09wani@gmail.com>
2017-12-20 13:32:20 +0530

    Noting serious, minor typos in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
 import { MyApp } from './app.component';
+import { MenuPage } from '../pages/menu/menu';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { ProfilePage } from '../pages/profile/profile';
-import { MenuPage } from '../pages/menu/menu';
-import { PostDataProvider } from '../providers/post-data/post-data';
 
 
 @NgModule({
